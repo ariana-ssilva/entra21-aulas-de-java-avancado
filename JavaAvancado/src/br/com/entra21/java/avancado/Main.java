0aa79a7a8d17326fc4a0505cc519ce7953b069bd
ariana-ssilva <arianamoreira5021@gmail.com>
2022-06-07 18:35:33 -0300

    updating repository

--- JavaAvancado/src/br/com/entra21/java/avancado/Main.java
+++ JavaAvancado/src/br/com/entra21/java/avancado/Main.java
@@ -1,10 +1,16 @@
 package br.com.entra21.java.avancado;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
+import br.com.entra21.java.avancado.aula01.Aula01;
 import br.com.entra21.java.avancado.aula01.enuns.Enuns;
 import br.com.entra21.java.avancado.aula01.wrappers.Wrappers;
-import br.com.entra21.java.avancado.aula02.collections.list.AprenderList;
+import br.com.entra21.java.avancado.aula02.Aula02;
+import br.com.entra21.java.avancado.aula03.Aula03;
+import br.com.entra21.java.avancado.aula03.anotacoes.Responsavel;
+import br.com.entra21.java.avancado.aula04.Aula04;
 
 public class Main {
 
@@ -22,19 +28,20 @@ public class Main {
 				System.out.println("Até a proxima!");
 				break;
 			case 1:
-				Wrappers.aprender();
+				Aula01.aprender();
 				break;
 			case 2:
-				Enuns.aprender();
+				Aula02.aprender();
 				break;
 			case 3:
-				AprenderList.aprender();
+				Aula03.aprender();
 				break;
 			case 4:
-				pedirPaciencia();
+				new Aula04("Aula04 - Collections - SET - HashSet", new ArrayList<>(Arrays.asList("Definir","CRUD"))).aprender();
 				break;
 			case 5:
-				pedirPaciencia();
+				new Aula04("Aula05 - Collections - MAP - HashMap", null).aprender();
+				
 				break;
 			case 6:
 				pedirPaciencia();
@@ -65,17 +72,14 @@ public class Main {
 	private static String montarMenu() {
 		String menu = "Módulo sobre JAVA AVANÇADO:";
 		menu += "\n  0 - Sair";
-		menu += "\n  1 - Aula 01 (pronto)- Classes Wrapper";// juntos 01
-		menu += "\n  2 - Aula 01 (pronto)- ENUM";// juntos 01
-
-		menu += "\n  3 - Aula 02 - Collections - LIST";// somente 02
-		menu += "\n  4 - Aula 03 - Annotations";// juntos 03
-		menu += "\n  5 - Aula 03 - Datas";// juntos 03
-		menu += "\n  6 - Aula 04 - Collections - SET";// somente 04
+		menu += "\n  1 - Aula 01  ENUM e Classes Wrapper";
+		menu += "\n  2 - Aula 02 - Collections - LIST";// somente 02
+		menu += "\n  4 - Aula 03 - Annotations e Datas";// juntos 03
+		menu += "\n  5 - Aula 04 - Collections - SET";// somente 04
 		menu += "\n  6 - Aula 05 - Collections - MAP";// somente 05
-		menu += "\n  8 - Aula 06 - Generics";// somente 06
-		menu += "\n  9 - Aula 07 - Exceções e tratamento de erros";// somente 07
-		menu += "\n 10 - Aula 08 - Funções Lambda"; // somente 08
+		menu += "\n  7 - Aula 06 - Generics";// somente 06
+		menu += "\n  8 - Aula 07 - Exceções e tratamento de erros";// somente 07
+		menu += "\n  9 - Aula 08 - Funções Lambda"; // somente 08
 		menu += "\n--------------------------------------------------------";
 		menu += "\n Escolha uma opção para aprender:";
 		menu += "\n--------------------------------------------------------";
@@ -83,6 +87,7 @@ public class Main {
 		return menu;
 	}
 
+	@Responsavel(ordemPrioridade = Responsavel.OrdemPrioridade.DOCUMENTAR, item = "Mostrar que não ta na hora", quemAssume = "Rubem Oliota")
 	private static void pedirPaciencia() {
 		System.out.println("CALMA CALMA CALMA, muita CALMA... ainda não esta pronto.");
 		System.out.println("--------------------------------------------------------");
